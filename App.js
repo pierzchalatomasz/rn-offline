6e37bf9292dece52b72dd5ae5bed366c60f367a7
Piotr Pikula <piotr.pikula@brainhub.pl>
2020-02-03 12:31:28 +0100

    added splash screen

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { ReduxNetworkProvider } from 'react-native-offline';
@@ -14,9 +14,16 @@ import { ReduxNetworkProvider } from 'react-native-offline';
 import configureStore from './configureStore';
 import Sample from './src/components/Sample/Sample';
 
+import SplashScreen from 'react-native-splash-screen';
+
 const { store, persistor } = configureStore();
 
-const App = () => (
+const App = () => { 
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+  
+  return (
   <Provider store={store}>
     <ReduxNetworkProvider
       pingTimeout={10000}
@@ -28,5 +35,5 @@ const App = () => (
     </ReduxNetworkProvider>
   </Provider>
 );
-
+}
 export default App;
